1ca00d3de556d985ab0a5b336731231957d94836
Michel Sommer <skruen@gmail.com>
2022-05-20 10:15:15 +0200

    temp

--- JidGrid/app/src/main/java/dk/seahawk/jidgrid/ui/locator/LocatorFragment.java
+++ JidGrid/app/src/main/java/dk/seahawk/jidgrid/ui/locator/LocatorFragment.java
@@ -1,5 +1,7 @@
 package dk.seahawk.jidgrid.ui.locator;
 
+import android.annotation.SuppressLint;
+import android.location.Location;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,28 +12,94 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+
+import dk.seahawk.jidgrid.algorithm.GridAlgorithm;
+import dk.seahawk.jidgrid.algorithm.GridAlgorithmInterface;
 import dk.seahawk.jidgrid.databinding.FragmentLocatorBinding;
 
 // View
 public class LocatorFragment extends Fragment {
 
+    // private AddressResultReceiver mResultReceiver;
+    // removed here because cause wrong code when implemented and
+    // its not necessary like the author says
+
+    //Define fields for Google API Client
+    private FusedLocationProviderClient mFusedLocationClient;
+    private Location lastLocation;
+    private LocationCallback mLocationCallback;
+
+    private GridAlgorithmInterface gridAlgorithm;
+
     private FragmentLocatorBinding binding;
 
+    TextView jidField,lonField,latField,altField;
+
+    @SuppressLint("MissingPermission")
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         LocatorViewModel locatorViewModel = new ViewModelProvider(this).get(LocatorViewModel.class);
-
+        gridAlgorithm = new GridAlgorithm();
         binding = FragmentLocatorBinding.inflate(inflater, container, false);
         View root = binding.getRoot();
 
-        final TextView jidField = binding.txtJid;
-        final TextView lonField = binding.txtLongitude;
-        final TextView latField = binding.txtLatitude;
-        final TextView altField = binding.txtAltitude;
+    /*
+        TextView jidField = binding.txtJid;
+        TextView lonField = binding.txtLongitude;
+        TextView latField = binding.txtLatitude;
+        TextView altField = binding.txtAltitude;
 
         locatorViewModel.getJid().observe(getViewLifecycleOwner(), jidField::setText);
         locatorViewModel.getLon().observe(getViewLifecycleOwner(), lonField::setText);
         locatorViewModel.getLat().observe(getViewLifecycleOwner(), latField::setText);
         locatorViewModel.getAlt().observe(getViewLifecycleOwner(), altField::setText);
+
+
+        TextView jidField = (TextView)root.findViewById(R.Id.txt_jid);
+        TextView lonField = (TextView)root.findViewById(R.id.txt_latitude);
+        TextView latField = (TextView)root.findViewById(R.id.txt_longitude);
+        TextView altField = (TextView)root.findViewById(R.id.txt_altitude);
+
+*/
+
+
+        try {
+            mFusedLocationClient = LocationServices.getFusedLocationProviderClient(getActivity());
+            mFusedLocationClient.getLastLocation().addOnSuccessListener(getActivity(), location -> {
+                        // Got last known location. In some rare situations this can be null.
+                        if (location != null) {
+
+                            // Logic to handle location object
+                            jidField.setText(gridAlgorithm.getGridLocation(location));
+                            lonField.setText(String.valueOf(location.getLongitude()));
+                            latField.setText(String.valueOf(location.getLatitude()));
+                            altField.setText(String.valueOf(location.getAltitude()));
+                        }
+                    });
+
+            mLocationCallback = new LocationCallback() {
+                @Override
+                public void onLocationResult(LocationResult locationResult) {
+                    for (Location location : locationResult.getLocations()) {
+                        // Update UI with location data
+                        jidField.setText(gridAlgorithm.getGridLocation(location));
+                        lonField.setText(String.valueOf(location.getLongitude()));
+                        latField.setText(String.valueOf(location.getLatitude()));
+                        altField.setText(String.valueOf(location.getAltitude()));
+                    }
+                }
+
+                ;
+            };
+        } catch (SecurityException ex) {
+            ex.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
 
         return root;
     }
--- JidGrid/app/src/main/java/dk/seahawk/jidgrid/ui/locator/LocatorViewModel.java
+++ JidGrid/app/src/main/java/dk/seahawk/jidgrid/ui/locator/LocatorViewModel.java
@@ -1,44 +1,71 @@
 package dk.seahawk.jidgrid.ui.locator;
 
+import android.annotation.SuppressLint;
+import android.app.Application;
 import android.location.Location;
 
 import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationListener;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 
+import java.util.concurrent.Executor;
+
+import dk.seahawk.jidgrid.MainActivity;
 import dk.seahawk.jidgrid.algorithm.GridAlgorithm;
 import dk.seahawk.jidgrid.algorithm.GridAlgorithmInterface;
 
-public class LocatorViewModel extends ViewModel implements LocationListener {
+public class LocatorViewModel extends AndroidViewModel {
 
     // The geographical location where the device is currently located. That is, the last-known
     // location retrieved by the Fused Location Provider.
-    private Location lastKnownLocation;
+ /*   private Location lastKnownLocation;
     private GridAlgorithmInterface gridAlgorithm;
 
     private final MutableLiveData<String> jidText;
     private final MutableLiveData<String> lonText;
     private final MutableLiveData<String> latText;
-    private final MutableLiveData<String> altText;
+    private final MutableLiveData<String> altText;*/
 
-    public LocatorViewModel() {
-        gridAlgorithm = new GridAlgorithm();
+    @SuppressLint("MissingPermission")
+    public LocatorViewModel(Application application) {
+        super(application);
+    /*    gridAlgorithm = new GridAlgorithm();
 
         jidText = new MutableLiveData<>();
         lonText = new MutableLiveData<>();
         latText = new MutableLiveData<>();
-        altText = new MutableLiveData<>();
-
+        altText = new MutableLiveData<>();  */
+/*
         //TODO NULL Pointer Exception, Location is not set
         jidText.setValue("init. grid locator");
         lonText.setValue("init. longitude");
         latText.setValue("init. latitude");
         altText.setValue("init. altitude");
-    }
+*/
+        /*
+                    lastKnownLocation = location;
 
+                    jidText.setValue(gridAlgorithm.getGridLocation(lastKnownLocation));
+                    lonText.setValue(String.valueOf(lastKnownLocation.getLongitude()));
+                    latText.setValue(String.valueOf(lastKnownLocation.getLatitude()));
+                    altText.setValue(String.valueOf(lastKnownLocation.getAltitude()));
+        */
+
+        // mResultReceiver = new AddressResultReceiver(null);
+        // cemented as above explained
+
+
+
+    }
+/*
     public LiveData<String> getJid() {
         return jidText;
     }
@@ -54,15 +81,5 @@ public class LocatorViewModel extends ViewModel implements LocationListener {
     public LiveData<String> getAlt() {
         return altText;
     }
-
-    @Override
-    public void onLocationChanged(@NonNull Location location) {
-        lastKnownLocation = location;
-
-        //TODO NULL Pointer Exception, Location is not set
-        jidText.setValue(gridAlgorithm.getGridLocation(lastKnownLocation));
-        lonText.setValue(String.valueOf(lastKnownLocation.getLongitude()));
-        latText.setValue(String.valueOf(lastKnownLocation.getLatitude()));
-        altText.setValue(String.valueOf(lastKnownLocation.getAltitude()));
-    }
+*/
 }
